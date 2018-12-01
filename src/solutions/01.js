923191c230dde78cf76a612a9a7c93a7261f6340
James Monger <jameskmonger@hotmail.co.uk>
2018-12-01 15:02:27 +0000

    Solve day 1 part 2

--- src/solutions/01.js
+++ src/solutions/01.js
@@ -1,5 +1,31 @@
 const partOne = (input) => input.split("\n").reduce((acc, cur) => acc + parseInt(cur), 0);
 
+const partTwo = (input) => {
+    const instructions = input.split("\n");
+    const counts = [];
+
+    // set up initial values
+    let value = 0;
+    counts[0] = 1;
+
+    while (true) {
+        for (let i of instructions) {
+            value += parseInt(i);
+
+            if (counts[value] === undefined) {
+                counts[value] = 0;
+            }
+            
+            counts[value]++;
+
+            if (counts[value] == 2) {
+                return value;
+            }
+        }
+    }
+};
+
 module.exports = {
-    partOne: partOne
+    partOne: partOne,
+    partTwo: partTwo
 };
