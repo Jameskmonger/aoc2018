8cbb3bc98a91ff6e7a46a0dce255bd3d6e505a31
James Monger <jameskmonger@hotmail.co.uk>
2018-12-05 07:32:26 +0000

    Refactor our reactPolymers

--- src/solutions/05.js
+++ src/solutions/05.js
@@ -1,10 +1,10 @@
 const ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const generateRegex = (char) => {
+const generatePairRegex = (char) => {
     const upper = char.toUpperCase();
     return RegExp(`(${char}${upper})|(${upper}${char})`, "g");
 };
 
-const partOne = (input) => {
+const reactPolymers = (input) => {
     let result = input;
     let changesMade = true;
 
@@ -12,7 +12,7 @@ const partOne = (input) => {
         const startLength = result.length;
 
         for (const char of ALPHABET) {
-            const regex = generateRegex(char);
+            const regex = generatePairRegex(char);
             
             result = result.replace(regex, "");
         }
@@ -20,8 +20,10 @@ const partOne = (input) => {
         changesMade = startLength !== result.length;
     }
 
-    return result.length;
+    return result;
 };
+
+const partOne = (input) => reactPolymers(input).length;
 
 module.exports = {
     partOne: partOne
