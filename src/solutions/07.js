96f9e03c2578799e4df5596ca74e71709037f820
James Monger <jameskmonger@hotmail.co.uk>
2018-12-09 16:13:23 +0000

    Solve day 7 part 2

--- src/solutions/07.js
+++ src/solutions/07.js
@@ -14,7 +14,8 @@ const getStep = (steps, id) => {
         steps[id] = {
             id: id,
             completed: false,
-            dependencies: []
+            dependencies: [],
+            locked: false
         };
     }
 
@@ -22,7 +23,7 @@ const getStep = (steps, id) => {
 };
 
 const getNextAvailableStep = (steps) => {
-    const uncompletedSteps = Object.values(steps).filter(s => s.completed === false);
+    const uncompletedSteps = Object.values(steps).filter(s => s.completed === false && s.locked === false);
 
     if (uncompletedSteps.length === 0) {
         return null;
@@ -47,10 +48,10 @@ const getNextAvailableStep = (steps) => {
     return null;
 };
 
-const partOne = (input) => {
+const getSteps = (input) => {
     const lines = input.split("\n").map(parseLine);
 
-    const steps = lines.reduce((acc, { dependencyId, stepId }) => {
+    return lines.reduce((acc, { dependencyId, stepId }) => {
         const step = getStep(acc, stepId);
         getStep(acc, dependencyId); // make the dependency too
 
@@ -58,6 +59,10 @@ const partOne = (input) => {
 
         return acc;
     }, []);
+}
+
+const partOne = (input) => {
+    const steps = getSteps(input);
 
     const result = [];
 
@@ -75,6 +80,70 @@ const partOne = (input) => {
     return result.join("");
 };
 
+const getStepLength = (step) => step.id.charCodeAt(0) - 64;
+
+const createWorkers = (count) => {
+    const workers = [];
+
+    for (let i = 0; i < count; i++) {
+        workers.push({
+            currentStep: null,
+            finishesAt: null
+        });
+    }
+
+    return workers;
+};
+
+const getAvailableWorker = (workers, step) => {
+    return workers.find(w => w.currentStep === null) || null;
+};
+
+const partTwo = (input) => {
+    const steps = getSteps(input);
+    const workers = createWorkers(5);
+    const BASE_STEP_LENGTH = 60;
+    let stepCount = 0;
+
+    while (true) {
+        if (Object.values(steps).every(s => s.completed === true)) {
+            break;
+        }
+
+        const finishingWorkers = workers.filter(w => w.currentStep !== null && w.finishesAt === stepCount);
+
+        finishingWorkers.forEach(w => {
+            w.currentStep.completed = true;
+            w.currentStep = null;
+            w.finishesAt = null;
+        });
+
+        const availableWorkers = workers.filter(w => w.currentStep === null);
+
+        if (availableWorkers.length === 0) {
+            stepCount++;
+            continue;
+        }
+
+        availableWorkers.forEach(w => {
+            const step = getNextAvailableStep(steps);
+
+            if (step === null) {
+                return;
+            }
+
+            step.locked = true;
+            w.currentStep = step;
+            w.finishesAt = stepCount + BASE_STEP_LENGTH + getStepLength(step);
+        });
+
+        stepCount++;
+    }
+
+    return stepCount - 1;
+};
+
 module.exports = {
-    partOne: partOne
+    partOne: partOne,
+    partTwo: partTwo
 };
