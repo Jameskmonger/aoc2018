17465e17b0ccbeb8ed9eba9544b8e35129aac623
James Monger <jameskmonger@hotmail.co.uk>
2018-12-09 16:57:39 +0000

    Solve day 8 part 2

--- src/solutions/08.js
+++ src/solutions/08.js
@@ -26,6 +26,64 @@ const partOne = (input) => {
     return totalMetadata;
 };
 
+const readNodeTree = (numbers) => {
+    const readNode = (pointer) => {
+        const childNodeCount = numbers[pointer++];
+        const metadataCount = numbers[pointer++];
+
+        const metadata = [];
+        const children = [];
+
+        for (let i = 0; i < childNodeCount; i++) {
+            const read = readNode(pointer);
+            pointer = read.pointer;
+            children.push(read.node);
+        }
+
+        for (let m = 0; m < metadataCount; m++) {
+            metadata.push(numbers[pointer++]);
+        }
+
+        return {
+            pointer,
+
+            node: {
+                metadata,
+                children
+            }
+        };
+    };
+
+    const { node } = readNode(0);
+
+    return node;
+};
+
+const getNodeValue = (node) => {
+    if (node.children.length === 0) {
+        return node.metadata.reduce((acc, cur) => acc + cur, 0);
+    }
+
+    return node.metadata.reduce((acc, cur) => {
+        const child = node.children[cur - 1];
+
+        if (child === undefined) {
+            return acc;
+        }
+
+        return acc + getNodeValue(child);
+    }, 0);
+};
+
+const partTwo = (input) => {
+    const numbers = input.split(" ").map(i => parseInt(i));
+
+    const tree = readNodeTree(numbers);
+
+    return getNodeValue(tree);
+}
+
 module.exports = {
-    partOne: partOne
+    partOne: partOne,
+    partTwo: partTwo
 };
